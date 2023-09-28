657ff0167996ce6d2df764c0d6e829fb8887d0c8
Lokendra Singh <thejaduijin@gmail.com>
2023-09-28 19:07:20 +0530

    nav link created

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import HeroSection from "./components/HeroSection"
+import NavBar from "./components/NavBar"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
+      <NavBar />
       <div className="container mx-auto px-12 py-4">
         <HeroSection />
       </div>
