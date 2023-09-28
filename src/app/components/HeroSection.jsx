234490ee4b22fbaf5c87d621046cd8f7122ad6f7
Lokendra Singh <thejaduijin@gmail.com>
2023-09-28 18:32:46 +0530

    hero section completed

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,18 +1,45 @@
+"use client"
 import React from 'react';
 import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
 
 function HeroSection() {
     return (
         <section>
-            <div className='grid grid-cols-1 lg:grid-cols-12'>
-                <div className='col-span-7 place-self-center'>
-                    <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>Hello, I'm Lokendra</h1>
-                    <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
+            <div className='grid grid-cols-1 sm:grid-cols-12'>
+                <div className='col-span-7 place-self-center text-center sm:text-left'>
+                    <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+                            Hello, I'm{""}
+                        </span>
+                        <br></br>
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'Lokendra',
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'Web Developer',
+                                1000,
+                                // 'Software Developer',
+                                // 1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
                         The leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s.
                     </p>
                     <div>
-                        <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
-                        <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
+                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
+                            Hire Me
+                        </button>
+                        <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
+                            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+                                Download CV
+                            </span>
+                        </button>
                     </div>
                 </div>
                 <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
@@ -31,4 +58,4 @@ function HeroSection() {
     )
 }
 
-export default HeroSection
+export default HeroSection;
