49ec0aa2aa3d7ddf5f2579c29cae2d3f4dc596ec
Lokendra Singh <thejaduijin@gmail.com>
2023-10-26 18:32:55 +0530

    project section images

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -53,7 +53,7 @@ function AboutSection() {
         <section className='text-white'>
             <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
                 <Image
-                    src="/dev09.webp"
+                    src="/images/ab1.png"
                     width={500}
                     height={500}
                     alt='image'
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -4,18 +4,18 @@ import ProjectCard from './ProjectCard';
 const projectsData = [
     {
         id: 1,
-        title: "React Portfolio Website",
-        description: "Project 1 description",
-        image: "/images/projects/d1.png",
+        title: "HTML Portfolio Website",
+        description: "Portfolio Website build using HTML..",
+        image: "/images/projects/a4.png",
         tag: ["All", "Web"],
         gitUrl: "/",
         previewUrl: "/",
     },
     {
         id: 2,
-        title: "Potography Portfolio Website",
-        description: "Project 2 description",
-        image: "/images/projects/dev03.png",
+        title: "React Crypto Tracker Website",
+        description: "Tracks crypto prices build using React..",
+        image: "/images/projects/c4.webp",
         tag: ["All", "Web"],
         gitUrl: "/",
         previewUrl: "/",
@@ -23,35 +23,35 @@ const projectsData = [
     {
         id: 3,
         title: "E-commerce Application",
-        description: "Project 3 description",
-        image: "/images/projects/dev03.png",
+        description: "E-commerce website name Shopping Cart build using React..",
+        image: "/images/projects/e3.png",
         tag: ["All", "Web"],
         gitUrl: "/",
         previewUrl: "/",
     },
     {
         id: 4,
-        title: "Food Ordering Application",
-        description: "Project 4 description",
-        image: "/images/projects/dev03.png",
+        title: "My Blog Site",
+        description: "Daily news reading site by React..",
+        image: "/images/projects/b1.png",
         tag: ["All", "Mobile"],
         gitUrl: "/",
         previewUrl: "/",
     },
     {
         id: 5,
-        title: "React Firebase Template",
-        description: "Authentication and CRUD operations",
-        image: "/images/projects/dev03.png",
+        title: "Movies Hub",
+        description: "Movie watching site using React..",
+        image: "/images/projects/m1.png",
         tag: ["All", "Web"],
         gitUrl: "/",
         previewUrl: "/",
     },
     {
         id: 6,
-        title: "Full-stack Roadmap",
-        description: "Project 5 description",
-        image:"/images/projects/dev03.png",
+        title: "NEXT.js Portfolio Website",
+        description: "Portfolio Website build using NEXT.js..",
+        image:"/images/projects/p1.png",
         tag: ["All", "Web"],
         gitUrl: "/",
         previewUrl: "/",
@@ -64,7 +64,7 @@ const ProjectSection = () => {
             <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
                 My Projects
             </h2>
-            <div className='grid md:grid-col-3 gap-8 md:gap-12'>
+            <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
                 {
                     projectsData.map((project) => (
                         <ProjectCard
@@ -73,6 +73,8 @@ const ProjectSection = () => {
                             description={project.description}
                             imgUrl={project.image}
                             tag={project}
+                            gitUrl={project.gitUrl}
+                            previewUrl={project.previewUrl}
                         />
                     ))
                 }
