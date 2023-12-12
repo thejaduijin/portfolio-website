2d208a2e5aa5dca67a9e08b3fea1123c126bdc59
thejaduijin <lokendra.06.work@gmail.com>
2023-12-12 10:46:13 -0800

    email section done

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,22 @@
-import HeroSection from "./components/HeroSection"
-import NavBar from "./components/NavBar"
+import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
 import AboutSection from "./components/AboutSection";
 import ProjectSection from "./components/ProjectSection";
-
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
+import AchievementsSection from "./components/AchievementsSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
       <NavBar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AchievementsSection />
         <AboutSection />
         <ProjectSection />
+        <EmailSection />
       </div>
+      <Footer />
     </main>
-  )
+  );
 }
