6e23a19cd1a8ee3b5e0c4e328ba03cf95a3c4e3b
Lokendra Singh <lokendra.06.work@gamil.com>
2024-03-17 17:33:58 +0530

    text

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -42,7 +42,7 @@ function HeroSection() {
                             href="mailto:lokendra.06.work@gamil.com"
                             target="_blank"
                             className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
-                            HireMe
+                            Hire Me
                         </a>
                         {/* <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
                             <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
@@ -54,7 +54,7 @@ function HeroSection() {
                             target="_blank"
                             rel="noopener noreferrer"
                             className='px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
-                            DownloadCV
+                            Download CV
                             {/* <span className=' bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
                             </span> */}
                         </a>
