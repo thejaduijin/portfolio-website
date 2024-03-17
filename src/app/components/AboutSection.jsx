64e74516da0bd2b890321007b26d4b6463d2d263
Lokendra Singh <lokendra.06.work@gamil.com>
2024-03-17 19:04:05 +0530

    education added

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -23,8 +23,10 @@ const TAB_DATA = [
         id: "education",
         content: (
             <ul className="list-disc pl-2">
-                <li>Fullstack Academy of Code</li>
-                <li>University of California, Santa Cruz</li>
+                <li>Masters of Computer Application(MCA) from AKTU Lacknow.</li>
+                <li>Bachelor of Computer Application(BCA) from DBRAU Agra.</li>
+                <li>Intermediate from UP Board.</li>
+                <li>High School from UP Board.</li>
             </ul>
         ),
     },
@@ -33,8 +35,8 @@ const TAB_DATA = [
         id: "experience",
         content: (
             <ul className="list-disc pl-2">
-                <li>AWS Cloud Practitioner</li>
-                <li>Google Professional Cloud Developer</li>
+                <li>Casino Games Development</li>
+                <li>Web Development</li>
             </ul>
         ),
     },
